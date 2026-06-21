1b91eedfd6b61cf7d3332497630a2d024c3f14dc
SebTheSigma <sebwhitehand@gmail.com>
2026-06-21 21:13:13 +0100

    Update button textures and adjust button panel sizes

--- behavior/src/examples/example1.ts
+++ behavior/src/examples/example1.ts
@@ -18,8 +18,8 @@ export function ExampleMenu(player: Player): void {
         localiseImage: true,
         forceGlobalImageParent: false,
         button_textures: {
-            default_texture: "textures/ui/default_button",
-            hover_texture: "textures/ui/hover_button",
+            default_texture: "textures/ui/forms/default_button",
+            hover_texture: "textures/ui/forms/hover_button",
         },
         onClick() {
             console.log("Button 1 clicked");
@@ -33,27 +33,20 @@ export function ExampleMenu(player: Player): void {
         localiseImage: true,
         forceGlobalImageParent: false,
         button_textures: {
-            default_texture: "textures/ui/default_button",
-            hover_texture: "textures/ui/hover_button",
+            default_texture: "textures/ui/forms/default_button",
+            hover_texture: "textures/ui/forms/hover_button",
         }
     };
 
     const button1 = new Button(
-        new ButtonPanel(0, 0, "100% - 10px", "50%"),
+        new ButtonPanel(0, 0, "100% - 10px", "50% - 21.5px"),
         new Image("textures/items/compass_item", 0, -6, 30, 30),
         new Label("Hello World!", 1, "100% - 20px", 1),
         buttonOptions1
     );
 
     const button2 = new Button(
-        new ButtonPanel(0, 0, "50%", 31),
-        new Image("textures/items/compass_item", "100% - 30px", 1, 25, 25),
-        new Label("Hello World!", 5, "100% - 17px", 1),
-        buttonOptions2
-    );
-
-    const button3 = new Button(
-        new ButtonPanel(0, 0, 100, 31),
+        new ButtonPanel(0, 0, "50% - 7.5px", 31),
         new Image("textures/items/compass_item", "100% - 30px", 1, 25, 25),
         new Label("Hello World!", 5, "100% - 17px", 1),
         buttonOptions2
@@ -63,8 +56,8 @@ export function ExampleMenu(player: Player): void {
         button1,
         new Stacker(
             button2,
-            new Image("textures/items/compass_item", 0, 0, 45, 45),
-        ).gap(5).orientate("horizontal"),
+            button2
+        ).gap(6).orientate("horizontal"),
         button1
     ).gap(0).setOffset(5, 5));
     
