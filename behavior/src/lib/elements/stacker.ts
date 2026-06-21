5c672ebca688c4bf76f2c92cfd02deb069f3d890
SebTheSigma <sebwhitehand@gmail.com>
2026-06-21 21:50:28 +0100

    Refactor Stacker class by removing padding methods

--- behavior/src/lib/elements/stacker.ts
+++ behavior/src/lib/elements/stacker.ts
@@ -2,14 +2,7 @@ import { Element } from "../general/types";
 
 export class Stacker {
     public elements: Element[];
-    private padding: { top: number | string; bottom: number | string; left: number | string; right: number | string } = {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-    };
     public spacing: number | string = 0;
-    public shouldOccupySpace = true;
     public orientation: "horizontal" | "vertical" = "vertical";
     public x: number | string = 0;
     public y: number | string = 0;
@@ -31,34 +24,6 @@ export class Stacker {
         return this;
     }
 
-    public pad(padding: number | string) {
-        this.padding.top = padding;
-        this.padding.bottom = padding;
-        this.padding.left = padding;
-        this.padding.right = padding;
-
-        return this;
-    }
-    public padB(padding: number | string) {
-        this.padding.bottom = padding;
-
-        return this;
-    }
-    public padT(padding: number | string) {
-        this.padding.top = padding;
-
-        return this;
-    }
-    public padL(padding: number | string) {
-        this.padding.left = padding;
-
-        return this;
-    }
-    public padR(padding: number | string) {
-        this.padding.right = padding;
-
-        return this;
-    }
 
     public orientate(orientation: "horizontal" | "vertical") {
         this.orientation = orientation;
@@ -66,14 +31,9 @@ export class Stacker {
         return this;
     }
 
-    public occupy(occupy: boolean = true) {
-        this.shouldOccupySpace = occupy;
-    }
-
     public clone(): Stacker {
         const clone = new Stacker(...this.elements.map((element) => element.clone()));
 
-        clone.padding = { ...this.padding };
         clone.spacing = this.spacing;
         clone.orientation = this.orientation;
         clone.x = this.x;
