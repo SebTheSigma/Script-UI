8ff2cf481879a3b0fa8e864212b1c56e861df4b9
SebTheSigma <sebwhitehand@gmail.com>
2026-06-21 20:43:38 +0100

    Add default texture constants for UI elements

---
+++ behavior/src/lib/data.ts
@@ -0,0 +1,17 @@
+import { BodyTextures } from "./general/types";
+
+// ----------------------------------------!!!
+// Edit the default textures for your pack.
+// ----------------------------------------!!!
+
+export const DEFAULT_BUTTON_TEX = {
+    default_texture: "textures/ui/forms/default_button",
+    hover_texture: "textures/ui/forms/hover_button"
+}
+
+export const DEFAULT_PANEL_TEX: BodyTextures = {
+    body_texure: "textures/ui/forms/default_background",
+    header_texture: "textures/ui/forms/default_title_background"
+}
+
+export const BLANK_TEX = "textures/ui/blank";
